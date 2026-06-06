5c2cbba2a33f21683c59a88092ee4e60ce8e66a4
alka chavan <145902587+Alkaschavan@users.noreply.github.com>
2026-06-06 14:51:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const burger = document.getElementById('burger');
+const nav = document.getElementById('navlink');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
+
+
